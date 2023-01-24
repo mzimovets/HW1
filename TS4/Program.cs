5abec69df478d45a3915f6122c4ca8eead84a36b
Максим <zimovets3@yandex.ru>
2023-01-25 00:18:14 +0300

    Final

--- TS4/Program.cs
+++ TS4/Program.cs
@@ -1,14 +1,14 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
 
-System.Console.WriteLine("Введите число: ");
+System.Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int i = 0;
-
-while(i < number)
+for (int i = 1; i < number; i++)
 {
-    if ((i % 2) == 0)
+    if (i % 2 == 0)
     {
-        System.Console.WriteLine(i + ",F");
+        System.Console.Write(i + " ");
     }
 }
+
+
